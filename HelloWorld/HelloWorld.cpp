0e3fbc2ea3b66aa21204cfa4c6c7b1424839cd3f
Jimb0nda <48517231+Jimb0nda@users.noreply.github.com>
2021-10-21 17:39:06 +0100

    Add files via upload

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello, World!" << endl;
+  cout << "Testing the output" << endl;
+  cout << "1 last time" << endl;
+  return 0;
+}
